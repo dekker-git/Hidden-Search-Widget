bf5504b993e5d919f385ec10eb24308fcb18e6f4
Dan <122179463+dekker-git@users.noreply.github.com>
2023-01-26 17:15:49 -0700

    got the transition done, but could use more work

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const searchClick = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+btn.addEventListener('click', () => {
+        searchClick.classList.toggle('active');
+        input.focus();
+        console.log('button pressed');}
+       
+);
